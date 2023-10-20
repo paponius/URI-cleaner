c27406348dcd8b26a7c4e5df10e8637538e4aea4
paponius <palo.hmm@gmail.com>
2023-10-20 12:50:48 +0200

    SRI - Subresource_Integrity hashes
    sri
    
    SRI - Subresource_Integrity hashes
    
    test if greasyfork.org will accept

--- GM/URI-cleaner.user.js
+++ GM/URI-cleaner.user.js
@@ -9,10 +9,10 @@
 
 // ==UserScript==
 // @name         Fix URI and links
-// @namespace    http://www.ponius.com/uricl/
-// @description  Removes all tracking info and clutter from links. Keeps other parameters intact.
+// @namespace    https://github.com/paponius/URI-cleaner/
+// @description  Removes clutter from links. Like tracking data. description: https://github.com/paponius/URI-cleaner/
 // @author       papo
-// @version      1.9
+// @version      1.9.1
 // @license      GPLv2
 //// Sites
 //// match can not be http?:// nor http*://
@@ -25,9 +25,9 @@
 /// tampermonkey has more, but if used in GreaseMonkey, script will silently fail. e.g. document-body
 // @run-at       document-start
 // @grant        none
-// @require      http://www.ponius.com/uricl/src/NormalizeURIandLinks.js?v1.9
-// @require      http://www.ponius.com/uricl/src/IMDb.js?v1.9
-// @require      http://www.ponius.com/uricl/src/google.js?v1.9
-// @require      http://www.ponius.com/uricl/src/twitter.js?v1.9
+// @require      https://raw.githubusercontent.com/paponius/URI-cleaner/main/src/NormalizeURIandLinks.js#sha256=11d0a39307e577ec8c8ae3f4106c9c2f92ded4eaf4e42528421adc3f6e1258c5
+// @require      https://raw.githubusercontent.com/paponius/URI-cleaner/main/src/IMDb.js#md5=838d11defbafc70dd40abf57ca74532a
+// @require      https://raw.githubusercontent.com/paponius/URI-cleaner/main/src/google.js#md5=954ef7edc218096a47409d8b369e6562
+// @require      https://raw.githubusercontent.com/paponius/URI-cleaner/main/src/twitter.js#md5=8107cba4daf4c89745e55ec0221d5084
 // @noframes
 // ==/UserScript==
